e3f190dca9884cc97f0ce01d1329e5982b1975da
DexTerR33 <pusaw202@gmail.com>
2022-05-24 00:47:09 +0300

    lp onepage

---
+++ Adidas-R1 NMD/3d-slider-master/app.js
@@ -0,0 +1,44 @@
+function shiftLeft() {
+    const boxes = document.querySelectorAll(".box");
+    const tmpNode = boxes[0];
+    boxes[0].className = "box move-out-from-left";
+
+    setTimeout(function() {
+        if (boxes.length > 3) {
+            tmpNode.classList.add("box--hide");
+            boxes[5].className = "box move-to-position5-from-left";
+        }
+        boxes[1].className = "box move-to-position1-from-left";
+        boxes[2].className = "box move-to-position2-from-left";
+        boxes[3].className = "box move-to-position3-from-left";
+        boxes[4].className = "box move-to-position4-from-left";
+        boxes[0].remove();
+
+        document.querySelector(".cards__container").appendChild(tmpNode);
+
+    }, 500);
+
+}
+
+function shiftRight() {
+    const boxes = document.querySelectorAll(".box");
+    boxes[4].className = "box move-out-from-right";
+    setTimeout(function() {
+        const noOfCards = boxes.length;
+        if (noOfCards > 3) {
+            boxes[3].className = "box box--hide";
+        }
+
+        const tmpNode = boxes[noOfCards - 1];
+        tmpNode.classList.remove("box--hide");
+        boxes[noOfCards - 1].remove();
+        let parentObj = document.querySelector(".cards__container");
+        parentObj.insertBefore(tmpNode, parentObj.firstChild);
+        tmpNode.className = "box move-to-position1-from-right";
+        boxes[0].className = "box move-to-position2-from-right";
+        boxes[1].className = "box move-to-position3-from-right";
+        boxes[2].className = "box move-to-position4-from-right";
+        boxes[3].className = "box move-to-position5-from-right";
+    }, 500);
+
+}
