2e4dbd71dc19a09febdc7b0ec346aaf5eb88ce83
DexTerR33 <pusaw202@gmail.com>
2022-09-17 01:28:39 +0300

    update

--- jiraParse/jiras.js
+++ jiraParse/jiras.js
@@ -25,12 +25,12 @@
 
 
 
-// let dataLink = new Request('https://jira.rei-d-services.com/activity?maxResults=10&os_authType=basic&title=undefined');
-
+// let dataLink = new Request('https://jira.rei-d-services.com/activity?maxResults=10&os_username=marian.iordache&os_password=Juyv3fwrhyYMZyTp&title=undefined');
+// console.log(dataLink, "tableData")
 // async function getDataJson() {
 //     const response = await fetch(dataLink, {
-//             method: 'GET',
-//             mode: 'cors',
+//             method: 'POST',
+//             mode: 'no-cors',
 //             cache: "default",
 //             headers: {
 //                          'Content-type': 'application/json; charset=UTF-8'
@@ -44,15 +44,7 @@
 
 //         })
 
-//         .then((objectData) => {
-//             let tableData = '';
-//             objectData.map((values, index, itemsToRenderInTable) => {
-//                 let result = values.hasOwnProperty('stream');
-//             }).join("");
-//             console.log(objectData, "objectData")
-//             // true
-//             console.log(getDataJson, "getDataJson")
-//         })
+    
 //         .catch(error => {
 //             console.error('There has been a problem with your fetch operation:', error);
 //         });
@@ -79,29 +71,14 @@
 
  
 
-//     (function() {
-
-// $.getJSON( "./jira.json", function( data ) {
-  
-//     var items = [];
-//     console.log(items,"items local json");
-//     $.each( data, function( key, val ) {
-//       items.push( "<li id='" + key + "'>" + val + "</li>" );
-//     });
-   
-//     $( "<ul/>", {
-//       "class": "my-new-list",
-//       html: items.join( "" )
-//     }).appendTo( "body" );
-//   });
+    // (function() {
 
 
 
 
-
-// // Assign handlers immediately after making the request,
-// // and remember the jqxhr object for this request
-// var jqxhr = $.getJSON( "./output.json", function() {
+// Assign handlers immediately after making the request,
+// and remember the jqxhr object for this request
+// var jqxhr = $.getJSON( "https://jira.rei-d-services.com/activity?maxResults=10&os_username=marian.iordache&os_password=Juyv3fwrhyYMZyTp&title=undefined", function() {
 //     console.log( "success local json", jqxhr );
 //   })
 //     .done(function() {
@@ -123,8 +100,8 @@
 //   });
 
 
-//     //var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-// //     var flickerAPI = "https://jira.rei-d-services.com/activity?maxResults=10&os_authType=basic&title=undefined";
+// //     var flickerAPI = "https://jira.rei-d-services.com/activity?maxResults=10&os_username=marian.iordache&os_password=Juyv3fwrhyYMZyTp&title=undefined";
+
 
 // //     $.getJSON( flickerAPI, {
 // //       tags: "mount rainier",
@@ -135,8 +112,8 @@
 
 // //       credentials: 'include',
 // //         headers: {
-// //         // 'Content-Type': 'application/json'
-// //         'Content-Type': 'application/x-www-form-urlencoded',
+// //         'Content-Type': 'application/json'
+// //         // 'Content-Type': 'application/x-www-form-urlencoded',
 // //       },
 
 // //     })
@@ -150,13 +127,14 @@
       
 // //       });
 // // console.log("flickerAPI",flickerAPI)
-// // console.log("data",data)
+
 //     })();
 
 const fetchDataBtn = document.querySelector("#fetchdata");
 const result = document.querySelector("#result");
 console.log(result , "result2");
 console.log(fetchDataBtn , "fetchDataBtn");
+
 console.log( "1");
 // gets data from API and sets the content of #result div
 async function getData() {
@@ -164,85 +142,104 @@ async function getData() {
   result.innerText = "Loading....";
   console.log(result, "result");
   try {
-    const res = await fetch("https://jira.rei-d-services.com/activity?maxResults=10&os_authType=basic&title=undefined");
+    const res = await fetch("https://jira.rei-d-services.com/activity?maxResults=10&os_username=marian.iordache&os_password=Juyv3fwrhyYMZyTp&title=undefined");
+    
+   
    
     const jsonResult = await res.json();
-    result.innerText = JSON.stringify(jsonResult, null, 2);
+    // result.innerText = JSON.stringify(jsonResult, null, 2);
   } catch (error) {
     console.log(error);
   }
 
 }
 
-// add event listener for #fetchdata button
+// // add event listener for #fetchdata button
 fetchDataBtn.addEventListener("click", getData);  
 
-async function postData(url = 'https://jira.rei-d-services.com/activity?maxResults=10&os_authType=basic&title=undefined', data = {}) {
-    // Default options are marked with *
+// async function postData(url = 'https://jira.rei-d-services.com/activity?maxResults=10&os_username=marian.iordache&os_password=Juyv3fwrhyYMZyTp&title=undefined', data = {}) {
+//     // Default options are marked with *
 
-    const response = await fetch(url, {
-      method: 'POST', // GET, POST, PUT, DELETE, etc.
-      mode: 'no-cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //     // 'Content-Type': 'text/plain'
-    //     // 'Content-Type': 'application/x-www-form-urlencoded',
-    //   },
-      headers: {
-        Authorization : 'Basic c2thcmFrYXM6NjU0MzIx',
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        Key: '1',
-        Type : 'asd',
-        Value : 'sdf'
-      }),
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    console.log(response, "response");
-    // return response.json(); // parses JSON response into native JavaScript objects
-  }
+//     const response = await fetch(url, {
+//       method: 'POST', // GET, POST, PUT, DELETE, etc.
+//       mode: 'no-cors', // no-cors, *cors, same-origin
+//       cache: 'no-cache', // default, no-cache, reload, force-cache, only-if-cached
+//       credentials: 'same-origin', // include, *same-origin, omit
+//       headers: {
+      
+//         'Content-Type': 'text/plain'
+//         // 'Content-Type': 'application/x-www-form-urlencoded',
+//       },
+    
+   
+    
+//       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//       body: JSON.stringify(data) // body data type must match "Content-Type" header
+//     });
+//     console.log(response, "response");
+//     // return response.json(); // parses JSON response into native JavaScript objects
+//   }
  
-  postData('https://jira.rei-d-services.com/activity?maxResults=10&os_authType=basic&title=undefined', { answer: 42 })
-    .then((data) => {
-      console.log(data, "data2"); // JSON data parsed by `data.json()` call
+//   postData('https://jira.rei-d-services.com/activity?maxResults=10&os_username=marian.iordache&os_password=Juyv3fwrhyYMZyTp&title=undefined', { answer: 42 })
+//     .then((data) => {
+//       console.log(data, "data2"); // JSON data parsed by `data.json()` call
+//     });
+
+
+
+
+
+
+    // function getRequest(url) { 
+    //     return fetch(url)
+    //         .then((res) => { 
+    //             if (res.ok) { // Checks server response (if there is one) 
+    //                 return res.json();
+    //             } else {
+    //                 throw new Error("Bad response");
+    //             }
+    //         });
+    // }
+    
+    // function getData(url) {
+        
+    //     getRequest(url)
+    //         .then(data => {
+    //             console.log(data); // the data
+    //         })
+    //         .catch(err => console.log(err)); // Catch handles an error
+    // }
+    
+    // getData("https://jira.rei-d-services.com/activity?maxResults=10&os_username=marian.iordache&os_password=Juyv3fwrhyYMZyTp&title=undefined");
+
+
+
+
+
+    var url= 'https://jira.rei-d-services.com/activity?maxResults=10&os_username=marian.iordache&os_password=Juyv3fwrhyYMZyTp&title=undefined';
+    $.ajax({
+        url: url,
+        type: 'POST',
+        mode: 'no-cors',
+        crossOrigin: false,
+        dataType: "jsonp",
+        headers: {
+                                     'Content-type': 'application/json; charset=UTF-8'
+                                },
+        xhrFields: { withCredentials: true },
+        accept: 'text/jsonp'
+    }).done(function (data) {
+        alert(data);                
+    }).fail(function (xhr, textStatus, error) {
+        var title, message;
+        switch (xhr.status) {
+            case 403:
+                title = xhr.responseJSON.errorSummary;
+                message = 'Please login to your server before running the test.';
+                break;
+            default:
+                title = 'Invalid URL or Cross-Origin Request Blocked';
+                message = 'You must explictly add this site (' + window.location.origin + ') to the list of allowed websites in your server.';
+                break;
+        }
     });
-
-
-
-
-
-
-    function getRequest(url) { 
-        return fetch(url)
-            .then((res) => { 
-                if (res.ok) { // Checks server response (if there is one) 
-                    return res.json();
-                } else {
-                    throw new Error("Bad response");
-                }
-            });
-    }
-    
-    function getData(url) {
-        getRequest(url)
-            .then(data => {
-                console.log(data); // the data
-            })
-            .catch(err => console.log(err)); // Catch handles an error
-    }
-    
-    getData('https://casino-promo.netbet.ro/scripts/cursa-streamurilor/data.php?user=');
-
-
-
-
-
-  
-
-
