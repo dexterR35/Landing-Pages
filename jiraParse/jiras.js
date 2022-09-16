6d66933de59928f0a970a8bec664eed9374331b1
DexTerR33 <pusaw202@gmail.com>
2022-09-17 01:31:21 +0300

    up js

--- jiraParse/jiras.js
+++ jiraParse/jiras.js
@@ -171,7 +171,7 @@ fetchDataBtn.addEventListener("click", getData);
 //         // 'Content-Type': 'application/x-www-form-urlencoded',
 //       },
     
-   
+   //
     
 //       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 //       body: JSON.stringify(data) // body data type must match "Content-Type" header
