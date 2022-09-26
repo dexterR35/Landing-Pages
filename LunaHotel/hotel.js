304a0e03351459db684fa9172d13b2f1788ce759
DexTerR33 <pusaw202@gmail.com>
2022-09-26 14:57:38 +0300

    update ex

--- LunaHotel/hotel.js
+++ LunaHotel/hotel.js
@@ -1,4 +1,3 @@
-
 // let div =  document.createElement('div');
 
 
@@ -70,8 +69,8 @@ btn_check.innerHTML = "check av";
 btn_check.name = "check-info_booking";
 btn_check.classList.add("btn-check", "btn-all")
 btn_check.addEventListener("click", function () {
-    alert("Button ckech is clicked");
-  });
+  alert("Button ckech is clicked");
+});
 
 
 //btn cancel
@@ -80,8 +79,8 @@ cancel_booking.innerHTML = "cancel";
 cancel_booking.name = "cancel-booking";
 cancel_booking.classList.add("btn-cancel_booking", "btn-all");
 cancel_booking.addEventListener("click", function () {
-    alert("Button cancel_booking is clicked");
-  });
+  alert("Button cancel_booking is clicked");
+});
 
 //btn make
 let make_booking = document.createElement('button');
@@ -89,8 +88,8 @@ make_booking.innerHTML = "make_booking";
 make_booking.name = "make-booking";
 make_booking.classList.add("btn-make_booking", "btn-all")
 make_booking.addEventListener("click", function () {
-    alert("Button make_booking is clicked");
-  });
+  alert("Button make_booking is clicked");
+});
 
 //append buttons
 btnDiv.append(make_booking, cancel_booking, btn_check);
@@ -118,17 +117,114 @@ hotelDiv2.appendChild(titles);
 
 
 const person = {
-    firstName: "John",
-    lastName : "Doe",
-    id       : 5566,
-    fullName : function() {
-      return this.firstName + " " + this.lastName;
-    }
-  };
-
-  const member = {
-    firstName:"Hege",
-    lastName: "Nilsen",
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
   }
-  
-  let fullName = person.fullName.bind(member);
+};
+
+const member = {
+  firstName: "Hege",
+  lastName: "Nilsen",
+}
+
+let fullName = person.fullName.bind(member);
+
+
+let today = new Date();
+let hour = today.getHours();
+var message = ", mtfucker"
+let greeting;
+if (hour > 18) {
+  greeting = "good evening"
+  message.textContent = "test"
+
+} else if (hour > 0) {
+  greeting = "good morning" + message;
+
+} else {
+  greeting = "welcome"
+  message.textContent = "test"
+  greeting = "good morning" + message;
+}
+document.write(greeting);
+
+
+let price;
+let quantity;
+let total;
+price = (5 + 1) * 6;
+quantity = 0.5 * (16 + 1);
+total = price * quantity;
+
+let divss = document.createElement("p");
+divss.className = "quantity";
+divss.textContent = "$" + total;
+hotelDiv2.appendChild(divss);
+
+
+
+
+
+let colors;
+colors = ['white', 'red', 'yellow'];
+let colorDiv = document.createElement('div');
+
+colorDiv.textContent = colors.length;
+hotelDiv2.appendChild(colorDiv);
+
+
+
+
+let msg = "sign up to recive";
+
+function updateMsg() {
+  let element = document.createElement("p");
+  element.textContent = msg;
+  hotelDiv2.appendChild(element);
+}
+updateMsg();
+
+
+function getArea(width, height, depth) {
+  let areaC = width * height;
+  let volume = width * height * depth;
+  let sizes = [areaC, volume];
+  console.log(depth, "depth");
+  console.log(volume, "volume");
+  console.log(areaC, "areaC");
+  console.log(sizes, "sizes");
+  return sizes;
+}
+
+
+let one = getArea(2, 4, 6)[0];
+let two = getArea(1, 3, 9)[1];
+
+console.log(one, "one");
+console.log(two, "two");
+
+
+
+var hotel = {
+  name: "luna",
+  rooms: 40,
+  booked: 25,
+  gym: true,
+  roomTypes: ["twin", "double", "suite"],
+  checkAvailability: function () {
+   
+    return this.rooms - this.booked;
+  },
+};
+let check = hotel.checkAvailability();
+console.log(check,"check anailability")
+
+console.log(hotel, "Hotel");
+
+
+let hotelName = document.createElement('p');
+hotelName.textContent = hotel.name;
+hotelDiv2.appendChild(hotelName);
