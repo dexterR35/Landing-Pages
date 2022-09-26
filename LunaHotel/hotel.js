77f4f44530f898b6d58d7f9521fd2325b0c83f3f
DexTerR33 <pusaw202@gmail.com>
2022-09-26 11:20:00 +0300

    luna

--- LunaHotel/hotel.js
+++ LunaHotel/hotel.js
@@ -125,3 +125,10 @@ const person = {
       return this.firstName + " " + this.lastName;
     }
   };
+
+  const member = {
+    firstName:"Hege",
+    lastName: "Nilsen",
+  }
+  
+  let fullName = person.fullName.bind(member);
