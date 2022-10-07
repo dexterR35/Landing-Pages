4caab2352ffff9e3b24f15d82e7a19f550cdcbcc
DexTerR33 <pusaw202@gmail.com>
2022-10-07 17:09:03 +0300

    update

--- sectionPage/test.js
+++ sectionPage/test.js
@@ -183,55 +183,55 @@ function boldString(str, substr) {
   }
 
 
-  var fruitsArray = [
-    { name: 'Banana', image: 'banana.jpg' }, 
-    { name: 'Apple', image: 'apple.jpg' }, 
-    { name: 'Orange', image: 'orange.jpg' }
-  ];
+//   var fruitsArray = [
+//     { name: 'Banana', image: 'banana.jpg' }, 
+//     { name: 'Apple', image: 'apple.jpg' }, 
+//     { name: 'Orange', image: 'orange.jpg' }
+//   ];
 
-  for (var i = 0; i < boldStuffs.length; i += 1) {
-    var fruit = getRandomItem(fruitsArray);
-    boldStuffs[i].innerHTML = fruit.name + '<img src="'+fruit.image+'">';
-}
+//   for (var i = 0; i < boldStuffs.length; i += 1) {
+//     var fruit = getRandomItem(fruitsArray);
+//     boldStuffs[i].innerHTML = fruit.name + '<img src="'+fruit.image+'">';
+// }
 
 
-var count =0;
-$(document).ready(function() {
-  if (window.File && window.FileList && window.FileReader) {
-    $("#files").on("change", function(e) {
+// var count =0;
+// $(document).ready(function() {
+//   if (window.File && window.FileList && window.FileReader) {
+//     $("#files").on("change", function(e) {
 
-      var files = e.target.files,
-        filesLength = files.length;
-        console.log(filesLength);
-        count++;
-      for (var i = 0; i < filesLength; i++) {
-        var f = files[i]
-        var fileReader = new FileReader();
-        fileReader.onload = (function(e) {
-          var file = e.target;
-          $("<span class=\"pip\">" +
-            "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
-            "<br/><span class=\"removeImg\">Remove image</span>" +
-            "</span>").insertAfter("#files");
-          $(".removeImg").click(function(){
-            $(this).parent(".pip").remove();
-          });
+//       var files = e.target.files,
+//         filesLength = files.length;
+//         console.log(filesLength);
+//         count++;
+//       for (var i = 0; i < filesLength; i++) {
+//         var f = files[i]
+//         var fileReader = new FileReader();
+//         fileReader.onload = (function(e) {
+//           var file = e.target;
+//           $("<span class=\"pip\">" +
+//             "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
+//             "<br/><span class=\"removeImg\">Remove image</span>" +
+//             "</span>").insertAfter("#files");
+//           $(".removeImg").click(function(){
+//             $(this).parent(".pip").remove();
+//           });
 
-          // Old code here
-          /*$("<img></img>", {
-            class: "imageThumb",
-            src: e.target.result,
-            title: file.name + " | Click to remove"
-          }).insertAfter("#files").click(function(){$(this).remove();});*/
+//           // Old code here
+//           /*$("<img></img>", {
+//             class: "imageThumb",
+//             src: e.target.result,
+//             title: file.name + " | Click to remove"
+//           }).insertAfter("#files").click(function(){$(this).remove();});*/
 
-        });
-        fileReader.readAsDataURL(f);
-      }
-    });
-  } else {
-    alert("Your browser doesn't support to File API")
-  }
-});
+//         });
+//         fileReader.readAsDataURL(f);
+//       }
+//     });
+//   } else {
+//     alert("Your browser doesn't support to File API")
+//   }
+// });
 
 
 
