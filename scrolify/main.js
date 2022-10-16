eb4a280ac0b13bb329397929026d9efc7582139a
DexTerR33 <pusaw202@gmail.com>
2022-10-17 01:28:05 +0300

    update

--- scrolify/main.js
+++ scrolify/main.js
@@ -2,6 +2,7 @@ $(function() {
     $.scrollify({
           section:".panel",
       scrollbars:false,
+      standardScrollElements:".forScrolling",
       before: function(i,panels) {
         var ref = panels[i].attr("data-section-name");
   
@@ -10,33 +11,33 @@ $(function() {
         // $(".pagination a[href=#" + ref + "]").addClass("active");
   
         /*
-        if(ref==="section_3") {
-          $(".section_3 .gallery0,.section_3 .gallery1,.section_3 .gallery2").addClass("moved");
+        if(ref==="inner_section_3") {
+          $(".inner_section_3 .gallery0,.inner_section_3 .gallery1,.inner_section_3 .gallery2").addClass("moved");
   
         }*/
   
         panels[i].find(".gallery0,.gallery1,.gallery2").addClass("moved");
    
   
-        if(ref==="section_2") {
-          $(".section_3").find(".gallery0,.gallery1,.gallery2").removeClass("moved");
-          $(".section_4 .gallery0").css("top",0);
+        if(ref==="inner_section_2") {
+          $(".inner_section_3").find(".gallery0,.gallery1,.gallery2").removeClass("moved");
+          $(".inner_section_4 .gallery0").css("top",0);
         }
-        if(ref==="section_3") {
-          $(".section_4 .content").removeClass("moved");
+        if(ref==="inner_section_3") {
+          $(".inner_section_4 .content").removeClass("moved");
           initialPosition();
         }
-        if(ref==="section_4") {
-          $(".section_4 .content").addClass("moved");
+        if(ref==="inner_section_4") {
+          $(".inner_section_4 .content").addClass("moved");
   
-          $(".section_4 .gallery0").css("top",0);
+          $(".inner_section_4 .gallery0").css("top",0);
         }
       },
       after:function(i,panels) {
         var ref = panels[i].attr("data-section-name");
   
         if(ref==="home") {
-          $(".section_2").find(".gallery0,.gallery1,.gallery2").removeClass("moved");
+          $(".inner_section_2").find(".gallery0,.gallery1,.gallery2").removeClass("moved");
         }
         for(var j = 0;j < panels.length;j++) {
           if(j>i) {
@@ -60,18 +61,28 @@ $(function() {
   
       var current = $.scrollify.current();
   
-      if(current.hasClass("section_4")===false) {
-        var height = parseInt($(".section_4").height());
-        var f = parseInt($(".section_3 .gallery1").height()) - 50;
+      if(current.hasClass("inner_section_4")===false) {
+        var height = parseInt($(".inner_section_4").height());
+        var f = parseInt($(".inner_section_3 .gallery1").height()) - 50;
         console.log(f,"f");
         console.log(f,"height");
   
         var top = 0 - (height*0.3) - (height-f);
-        $(".section_4 .gallery0").css("top",top);
+        $(".inner_section_4 .gallery0").css("top",top);
       } else {
-        $(".section_3").find(".gallery0,.gallery1,.gallery2").addClass("moved");
+        $(".inner_section_3").find(".gallery0,.gallery1,.gallery2").addClass("moved");
       }
   
     }
   });
+  $(function () {
   
+    $('.md-trigger').on('click', function() {
+      $('.md-modal').addClass('md-show');
+    });
+    
+    $('.md-close').on('click', function() {
+      $('.md-modal').removeClass('md-show');
+    });
+    
+  });
