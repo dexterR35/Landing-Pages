629c5ff492d72be59078003a780ab73f7530ad5d
DexTerR33 <pusaw202@gmail.com>
2023-02-28 18:50:28 +0200

    update

--- macOS_Luna_beta/template/pages/settings.js
+++ macOS_Luna_beta/template/pages/settings.js
@@ -1,39 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log('start');
-  
-  
-      var dropBtns = document.querySelectorAll(".dropdown-button");
-      console.log(dropBtns, "btnSho");
-  
-      function closeOpenItems() {
-        openMenus = document.querySelectorAll(".dropMenu-list");
-        openMenus.forEach(function (menus) {
-          menus.classList.remove("show");
-        });
-      }
-  
-      dropBtns.forEach(function (btn) {
-        btn.addEventListener("click", function (e) {
-          let dropContent = btn.querySelector(".dropMenu-list"),
-     
-            shouldOpen = !dropContent.classList.contains("show");
-            console.log(dropContent);
-          e.preventDefault();
-          // First close all open items.
-          closeOpenItems();
-          // Check if the clicked item should be opened. It is already closed at this point so no further action is required if it should be closed.
-          if (shouldOpen) {
-            // Open the clicked item.
-            dropContent.classList.add("show");
-          }
-          e.stopPropagation();
-        });
-      });
-      window.addEventListener("click", function (event) {
-        if (event.target != dropBtns) {
-          closeOpenItems();
-        }
-      });
-      console.log('middle');
+console.log("exportNavLink");
+export function openNavLink() {
+  let dropBtns = document.querySelectorAll(".dropdown-button");
+  console.log(dropBtns, "dropBtns");
+  dropBtns.forEach(function (btn) {
 
-    })
+    btn.addEventListener("click", function (e) {
+      let dropContent = btn.querySelector(".dropMenu-list"),
+        shouldOpen = !dropContent.classList.contains("show");
+      e.preventDefault();
+      // First close all open items.
+      closeOpenItems();
+      // Check if the clicked item should be opened. It is already closed at this point so no further action is required if it should be closed.
+      if (shouldOpen) {
+        console.log(shouldOpen, "boolean");
+        // Open the clicked item.
+        dropContent.classList.add("show");
+      }
+      e.stopPropagation();
+    });
+  });
+
+  function closeOpenItems() {
+    let openMenus = document.querySelectorAll(".dropMenu-list");
+    openMenus.forEach(function (menus) {
+      menus.classList.remove("show");
+    });
+  }
+  window.addEventListener("click", function (event) {
+    if (event.target != dropBtns) {
+      closeOpenItems();
+    }
+  });
+};
+
+let rangeInput = document.getElementById("rangeInput");
+// let output = document.getElementById("output");
+
+// output.innerHTML = rangeInput.value;
+
+// rangeInput.oninput = function() {
+//   output.innerHTML = this.value;
+// }
