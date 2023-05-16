dd72fc68b2fd89a5b5bdf00722d26d2f4139da0e
DexTerR33 <pusaw202@gmail.com>
2023-05-16 18:45:57 +0300

    update poker game

--- pokerGame/index.js
+++ pokerGame/index.js
@@ -1,110 +1,134 @@
 var vm = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     gather: true,
     state: "Press the cards to start a game",
     symbols: [
-      {label: "spades",symbol: "♠" },
-      {label: "hearts",symbol: "♥" },
-      {label: "diamonds",symbol: "♦" },
-      {label: "clubs",symbol: "♣" }
+      { label: "spades", image: "./png/green.png", symbol: "" },
+      { label: "hearts", image: "./png/orange.png", symbol: "" },
+      { label: "diamonds", image: "./png/red.png", symbol: "" },
+      { label: "777", image: "./png/full_red.png", symbol: "" },
     ],
-    cards:  [
+    cards: [
       {
-        id: 1,label: "spades",open: true
+        id: 1,
+        label: "spades",
+        open: true,
       },
       {
-        id: 2,label: "hearts", open: false
+        id: 2,
+        label: "hearts",
+        open: false,
       },
       {
-        id: 3,label: "clubs", open: false
+        id: 3,
+        label: "777",
+        open: false,
       },
       {
-        id: 4,label: "diamonds", open: false
-      }
+        id: 4,
+        label: "diamonds",
+        open: false,
+      },
     ],
     question: null,
     mode: "",
-    count: 0
+    count: 0,
+    pickOutState: false,
     
   },
   methods: {
-   shuffle(){
-  let newOrder = [1,2,3,4].sort((a,b)=> Math.random()>0.5?1:-1)
-  this.cards.forEach((card,cid)=> card.id=newOrder[cid])
-  console.log("Shuffle! " + this.count)
-  if( this.count++ < 6){
-    setTimeout(() => {
-      this.shuffle()
-    },300)
-  }else{
-    this.state="Please pick out "+ this.question.label + this.question.symbol
-    this.mode="Answer"
-    this.count = 0 // Reset the count after the game is finished
-  }
-},
-    turnAll(state){
-      this.cards.forEach(card => card.open=state)
-      console.log(this.cards,"test")
+    
+    getSymbolImage(label) {
+      const symbol = this.symbols.find((s) => s.label === label);
+      return symbol ? symbol.image : "";
     },
-    startGame(){
-      this.mode=""
-      this.question = this.symbols[parseInt(Math.random()*4)]
-      this.turnAll(false)
-      this.gather = true
-      this.state="Ready..."
-      setTimeout(()=> {
-        this.gather=false
-        this.state="Your mission is .."
-      },1000)
-      setTimeout(()=> {
-        this.turnAll(true)
-        this.state="Find "+this.question.label + this.question.symbol + "!"
-      },2000)
-      setTimeout(()=> {
-        this.turnAll(false)
-        this.state="Get ready..."
-      },4000)
-      this.count=0
-      setTimeout(()=>{
-        let startShuffle= () => {
-          this.shuffle()
-          console.log("Shuffle! "+this.count)
-          if( this.count++ < 6){
-            setTimeout(startShuffle,300)
-          }else{
-            this.state="Please pick out "+ this.question.label + this.question.symbol
-            this.mode="Answer"
-          }
-        }
-        startShuffle()
-      },6000)
+    shuffle() {
+      
+      let newOrder;
+        newOrder = [1, 2, 3, 4].sort(() => Math.random() - 0.5);
+        console.log("ordinea noua e:", newOrder);
+        this.cards.forEach((card, cid) => (card.id = newOrder[cid]));
       
     },
-    getSymbol(label){
-      let result = this.symbols.find(s => s.label == label)
-      return result?result.symbol:label
+    turnAll(state) {
+      this.cards.forEach((card) => (card.open = state));
     },
-    getCard(label){
-      return this.cards.find(card=> card.label==label)
+    startGame() {
+      const imagePath = "./png/asset.png";
+      this.mode = "";
+      this.question = this.symbols.find((symbol) => symbol.label === "777");
+      // this.question = this.symbols[parseInt(Math.random()*4)]
+      this.turnAll(false);
+      this.gather = true;
+      this.state = "lets play...";
+      setTimeout(() => {
+        this.gather = false;
+        this.state =
+        `Your mission is to follow the <img src="${imagePath}" alt="777"> card!`
+          console.log(imagePath, "img");
+          this.state = "lets play...";
+      }, 1000);
+      setTimeout(() => {
+        this.turnAll(true);
+
+          console.log(imagePath, "img");
+          this.state =
+            `Your mission is to follow the <img src="${imagePath}" alt="777"> card!` 
+      }, 2000);
+      setTimeout(() => {
+        this.turnAll(false);
+        this.state = "Get ready... ";
+      }, 4000);
+      this.count = 0;
+      setTimeout(() => {
+        this.pickOutState = false;
+        let startShuffle = () => {
+          this.shuffle();
+          console.log("Shuffle! " + this.count);
+          if (this.count++ < 6) {
+            setTimeout(startShuffle, 300);
+          } else {
+           
+              // const imagePath = "./png/asset.png";
+              console.log(imagePath, "img");
+              this.state =
+                `Please pick out <img src="${imagePath}" alt="777"> card!`
+              // this.state="Your mission is  follow the " + this.question.label + "card" + "!"
+       
+            // this.state =
+            //   "Please pick out " + this.question.label + this.question.symbol;
+            this.mode = "Answer";
+            this.pickOutState = true;
+          }
+        };
+        startShuffle();
+      }, 6000);
+    },
+    getSymbol(label) {
+      let result = this.symbols.find((s) => s.label == label);
+      return result ? result.symbol : label;
+    },
+    getCard(label) {
+      return this.cards.find((card) => card.label == label);
     },
     openCard(card) {
+      this.pickOutState = false;
       if (this.mode == "Answer") {
         card.open = !card.open;
         if (card.label == this.question.label) {
-          this.count++;
-          this.state = "You get the " + this.question.label + this.question.symbol + "!!!";
-          setTimeout(() => {
-            alert("win");
-          }, 1000);
+          if (this.question.label === "777") {
+            const imagePath = "./png/asset.png";
+            console.log(imagePath, "img");
+            this.state =
+              `congrats!! you found the <img src="${imagePath}" alt="777"> card!`
+            // this.state="Your mission is  follow the " + this.question.label + "card" + "!"
+          }
         } else {
-          this.state = "You lose!!!";
+          this.state = "You lose !!!";
           setTimeout(() => {
             let card = this.getCard(this.question.label);
             card.open = true;
-            setTimeout(() => {
-              alert("lose");
-            }, 1000);
           }, 1000);
         }
         setTimeout(() => {
@@ -113,10 +137,9 @@ var vm = new Vue({
       } else {
         this.startGame();
       }
-    }
+    },
   },
-  mounted(){
-    this.startGame()
-  }
-  
-})
+  mounted() {
+    this.startGame();
+  },
+});
