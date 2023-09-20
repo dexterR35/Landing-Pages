d2e7960e424c1f06e71210d35f727c68946392ee
dexterR35 <pusaw202@gmail.com>
2023-09-21 02:02:04 +0300

    update cookies

--- batalia-streamurilorv2/bataliaStreamerilor/index.js
+++ batalia-streamurilorv2/bataliaStreamerilor/index.js
@@ -5,38 +5,46 @@ $(document).ready(function () {
       name: "ana",
       points: "310",
       position: "3",
-      vote:"2",
+      vote: "2",
       challenge: false,
+      picture: {
+        _bgImg: false,
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/pacanela.png",
+      },
       social: {
         facebook: "ana facebook",
         insta: "ana insta",
         streaming: "link",
         website: "https://go-testing.netbet.ro/casino/?lp=bataliaStreamarilorv2&streamer=ana",
       },
-      bgPicture: "link",
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/pacanela.png",
     },
     {
       name: "stero",
       points: "120",
-      vote:"12",
+      vote: "12",
       position: "13",
       challenge: true,
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
         streaming: "link",
         website: "https://go-testing.netbet.ro/casino/?lp=bataliaStreamarilorv2&streamer=stero",
       },
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero2.png",
     },
     {
       name: "alin",
       points: "770",
-      vote:"12",
+      vote: "12",
       position: "131",
       challenge: false,
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/alin.png",
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/alin.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
@@ -49,7 +57,10 @@ $(document).ready(function () {
       points: "970",
       position: "113",
       challenge: true,
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/maria.png",
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/maria.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
@@ -61,7 +72,10 @@ $(document).ready(function () {
       name: "george",
       points: "1214",
       challenge: false,
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/george.png",
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/george.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
@@ -73,7 +87,10 @@ $(document).ready(function () {
       name: "daniel",
       points: "6453654",
       challenge: false,
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/daniel.png",
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/daniel.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
@@ -85,19 +102,26 @@ $(document).ready(function () {
       name: "stero",
       points: "120",
       challenge: true,
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero2.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
         streaming: "link",
         website: "https://go-testing.netbet.ro/casino/?lp=bataliaStreamarilorv2&streamer=stero",
       },
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero2.png",
+
     },
     {
       name: "daniel",
       points: "6453654",
       challenge: false,
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/daniel.png",
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero2.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
@@ -109,7 +133,10 @@ $(document).ready(function () {
       name: "daniel",
       points: "6453654",
       challenge: false,
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/daniel.png",
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero2.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
@@ -121,19 +148,26 @@ $(document).ready(function () {
       name: "stero",
       points: "120",
       challenge: true,
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero2.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
         streaming: "link",
         website: "https://go-testing.netbet.ro/casino/?lp=bataliaStreamarilorv2&streamer=stero",
       },
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero2.png",
+
     },
     {
       name: "daniel",
       points: "6453654",
       challenge: false,
-      avatar: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/daniel.png",
+      picture: {
+        _bgImg: "",
+        _avatarImg: "https://go-testing.netbet.ro/lp-asset/netbetro/casino/bataliaStreamarilorv2/avatar/stero2.png",
+      },
       social: {
         facebook: "{$asset}/social/facebook.png",
         insta: "stero insta",
@@ -173,19 +207,20 @@ $(document).ready(function () {
   ];
 
   // Function to generate a card
-  function generateCard(cardData) {
+  function generateCard(streamerData) {
+    // console.log(streamerData, "streamerData")
     const cardHtml = `
         <div class="col-lg-4 card-wrapp">
             <div class="card card-custom"; min-height:200px !important">
                 <div class="card-body text-center" >
-                    <a class="card-text text top-left" href="${cardData.social.website
+                    <a class="card-text text top-left" href="${streamerData.social.website
       }" target="_blank">Voteaza-ma</a>
-                    <p class="speech"> ${cardData.points + " " + "Voturi"
+                    <p class="speech"> ${streamerData.points + " " + "Voturi"
       }</p>
-                    <div class="card-image" style="background-image:url(${cardData.avatar
+                    <div class="card-image" style="background-image:url(${streamerData.picture._avatar
       })"></div>
                     <p class="card-text vote addVote">VOTAT</p>
-                    <h6 class="card-title text bottom-right">${cardData.name
+                    <h6 class="card-title text bottom-right">${streamerData.name
       }</h6>
                     
                 </div>
@@ -194,25 +229,25 @@ $(document).ready(function () {
     `;
     return cardHtml;
   }
-  let addvar = $("#badge").addClass("badge-success-alt")
+  // let addvar = $("#badge").addClass("badge-success-alt")
 
   // Function to generate table streamres
-  function generateDataTable(index, tableData) {
+  function generateDataTable(index, streamerData) {
     const tableAHtml = `<tr>
       <td>
           <div class="d-flex align-items-center">
-                  <div class="avatar-table avatar-blue mr-3">${(tableData.name).slice(0, 2).toUpperCase()}</div>
+                  <div class="avatar-table avatar-blue mr-3">${(streamerData.name).slice(0, 2).toUpperCase()}</div>
                   <div class="">
-                      <p class="font-weight-bold mb-0">${tableData.name}</p>
-                      <p class="text-muted mb-0">Voturi: ${tableData.vote}</a>
+                      <p class="font-weight-bold mb-0">${streamerData.name}</p>
+                      <p class="text-muted mb-0">Voturi: ${streamerData.vote}</a>
                   </div>
             </div>
       </td>
-      <td>${tableData.position}</td>
-      <td>${tableData.points}</td>
+      <td>${streamerData.position}</td>
+      <td>${streamerData.points}</td>
       <td>
-          <div class="badge ${tableData.challenge ? 'badge-success-alt' : 'text-bg-danger'}">
-          ${tableData.challenge ? "challenge" : "no challenge"}
+          <div class="badge ${streamerData.challenge ? 'badge-success-alt' : 'text-bg-danger'}">
+          ${streamerData.challenge ? "challenge" : "no challenge"}
           </div>
       </td>
      
@@ -224,32 +259,30 @@ $(document).ready(function () {
   function populateTable() {
     let dynamicCardBody = $("#dynamicCardBody");
     dynamicCardBody.empty(); // Clear any existing content
-
     let tableBody = $(".table-body");
     tableBody.empty(); // Clear any existing rows
-
-    streamers.forEach((cardData, index) => {
-      dynamicCardBody.append(generateCard(cardData));
-      tableBody.append(generateDataTable(index, cardData));
+    streamers.forEach((streamerData, index) => {
+      // console.log(streamerData, "card");
+      dynamicCardBody.append(generateCard(streamerData));
+      tableBody.append(generateDataTable(index, streamerData));
     });
   }
 
   //Function GamesName + manipulate the link
   function generateGames() {
     const gameImagesContainer = $("#gameImages");
-    for (let i = 0; i < gamesArray.length; i++) {
-      const gameName = gamesArray[i];
+    gamesArray.forEach((gameName) => {
       const img = $("<img>")
         .attr(
           "src",
           `https://img.netbet.ro/cdn-cgi/image/q=90,w=214,f=webp//gms/games/casino_new/preview/${gameName}.jpg`
         )
         .attr("alt", gameName);
-      const link = $("<a>").attr("href", "#").append(img);
+      const link = $("<a>").addClass("scroll-item-game").attr("href", "#").append(img);
       gameImagesContainer.append(link);
-    }
+    });
   }
-
+  
   // Function append bg card ,pos, name for swipper
   function appendStuff() {
     $(".slide-streamer").each(function (index) {
@@ -260,6 +293,7 @@ $(document).ready(function () {
       let boxSlideBottom = $(
         `<div class="bottom_swiper_box" data-swiper-parallax="-10"><span># </span><span>${streamer_pos}</span></div>`
       );
+ 
       $(this).append(boxSlideBottom);
       $(this).prepend(boxSlideTop);
       const bgImage = backgroundImages[index % backgroundImages.length];
@@ -282,7 +316,6 @@ $(document).ready(function () {
   appendStuff();
 });
 
-
 const swiperStr = new Swiper(".stream-slider", {
   effect: "coverflow",
   loop: true,
@@ -290,7 +323,7 @@ const swiperStr = new Swiper(".stream-slider", {
     delay: 0,
   },
   speed: 2 * 1000,
-  slidesPerView: "auto",
+  slidesPerView:"auto",
   parallax: true,
   touchRatio: 0.3,
   cache: true,
@@ -318,22 +351,83 @@ swiperStr.el.addEventListener("mouseleave", function () {
   swiperStr.autoplay.start();
 });
 
-const parallax4 = document.getElementById("parallax4");
-const bgPalalax = document.getElementById("background_p");
-const contentMiddle = document.getElementById("contentImg_p");
-const spikePalalax = document.getElementById("spike_p");
-const tesst = document.getElementById("tesst");
-// const gameImages = document.querySelector(".s2_providers");
-window.addEventListener("scroll", function () {
-  let offset = window.pageYOffset;
-  bgPalalax.style.backgroundPositionY = -offset * 0.1 + "px";
-  spikePalalax.style.transform = "translateY(" + (offset * 0.3 + "px)");
-  contentMiddle.style.transform = "translateY(" + (-offset * 0.1 + "px)");
-  // tesst.style.transform = "translateY(" + (offset * 0.04 + "px)");
+// const parallax4 = document.getElementById("parallax4");
+// const bgPalalax = document.getElementById("background_p");
+// const contentMiddle = document.getElementById("contentImg_p");
+// const spikePalalax = document.getElementById("spike_p");
+// const tesst = document.getElementById("tesst");
+// // const gameImages = document.querySelector(".s2_providers");
+// window.addEventListener("scroll", function () {
+//   let offset = window.pageYOffset;
+//   bgPalalax.style.backgroundPositionY = -offset * 0.1 + "px";
+//   spikePalalax.style.transform = "translateY(" + (offset * 0.3 + "px)");
+//   contentMiddle.style.transform = "translateY(" + (-offset * 0.1 + "px)");
+//   // tesst.style.transform = "translateY(" + (offset * 0.04 + "px)");
 
+// });
+
+
+// var head = $('.s2_providers')
+// $.scrollify({
+//   section:".section",
+//   scrollbars:false,
+//   interstitialSection:"",
+//   easing: "easeOutExpo",
+//   scrollSpeed: 300,
+//   touchScroll :true,
+//   offset :0,
+// 	standardScrollElements: ".normal-scroll",
+//   setHeights: true,
+//   overflowScroll: true,
+//   updateHash: false,
+//    before: function(i,section){
+//     console.log(i,"i")
+//     console.log(section,"panel")
+//       $('.section').removeClass('active')
+//       // section[i].find(".table-responsive").addClass("moved");
+    
+//     },
+//     after: function(){
+//       current = $.scrollify.current();
+//       current.addClass('active')
+//     }
+  
+// });
+const scrollContainer = document.querySelector("#fullpage");
+
+const scroll = new LocomotiveScroll({
+	el: scrollContainer,
+  smartphone: {
+    smooth: true
+},
+firefoxMultiplier:"",
+touchMultiplier:2,
+scrollFromAnywhere:true,
+reloadOnContextChange:true,
+resetNativeScroll:false,
+	smooth: true,
+	lerp: 0.05
+});
+
+scroll.on("scroll", (e) => {
+	scrollContainer.style.backgroundColor =
+		"hsl(" + 100 + e.scroll.y / 5 + ",40%,30%)";
+
+    const table = document.getElementById("gameImages"); // Selectați tabelul după ID
+
+    if (table) {
+      const skewValue = e.scroll.y / 9; // Ajustați acest număr pentru a controla intensitatea efectului de skew
+      table.style.transform = `skew(${skewValue / 25}deg)`; // Aplicați efectul de skew pe axa orizontală
+    }
 });
 
 $(document).ready(function () {
+  // $('.nav-link').on('click', function() {
+  //   var page = $(this).attr('href');
+  //   var speed = 1100;
+  //   $('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+  //   return false;
+  // });
   const commonOptions = {
     aaSorting: [],
     responsive: true,
@@ -348,6 +442,11 @@ $(document).ready(function () {
     },
     lengthChange: false,
     bFilter: false,
+  };
+
+  // Options for the "streamersTable" with all columns
+  const streamersTableOptions = {
+    ...commonOptions,
     columnDefs: [{
         responsivePriority: 1,
         targets: 0,
@@ -358,8 +457,23 @@ $(document).ready(function () {
       },
     ],
   };
-  $("#streamersTable").DataTable(commonOptions);
-  $("#usersTable").DataTable(commonOptions);
+
+  // Options for the "usersTable" without the "Challenge" column
+  const usersTableOptions = {
+    ...commonOptions,
+    columnDefs: [{
+        responsivePriority: 1,
+        targets: 0,
+      },
+      {
+        responsivePriority: 2,
+        targets: -1, // This targets the last column (which is "Challenge")
+        visible: false, // This hides the column
+      },
+    ],
+  };
+  $("#streamersTable").DataTable(streamersTableOptions);
+  $("#usersTable").DataTable(usersTableOptions);
 
 
   $("#streamersTable_wrapper").prepend(`<h4 class="text-center p-2 position-relative">Streameri</h4>`);
