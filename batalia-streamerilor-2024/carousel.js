6ee06d8703ebc068d1b42d9b76c8959a132e2a9c
dexterR35 <pusaw202@gmail.com>
2024-10-05 01:16:35 +0300

    fix terms

--- batalia-streamerilor-2024/carousel.js
+++ batalia-streamerilor-2024/carousel.js
@@ -1,55 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let gamesDataFetched = false; // Moved to outer scope
-  let gamesData = []; // Moved to outer scope
+  let gamesDataFetched = false;
+  let gamesData = []; 
 
   $(document).ready(init);
-
-  // Initialization function
   function init() {
     const slidesData = getSlidesData(); // Images for the first carousel
-
     // Initialize first carousel
     populateFirstCarousel(slidesData);
     initializeCarousel("#image-carousel", getFirstCarouselOptions());
-
     // Fetch and populate second carousel images
     fetchGames().then((games) => {
       gamesData = games;
       populateSecondCarousel(gamesData);
       initializeCarousel("#games-carousel", getSecondCarouselOptions());
     });
-
-    // Setup Scroll to Top button functionality
-    // setupScrollToTop();
-
     // Observe visibility of sections and footer
     observeSections();
   }
-
-  // Function to handle visibility of sections
+  // handle visibility of sections
   function handleSectionVisibility(entries) {
     entries.forEach((entry) => {
-      if (entry.isIntersecting || entry.target.getBoundingClientRect().top < window.innerHeight) {
+      if (
+        entry.isIntersecting ||
+        entry.target.getBoundingClientRect().top < window.innerHeight
+      ) {
         entry.target.classList.add("visible");
       } else {
         entry.target.classList.remove("visible");
       }
     });
   }
-
   // Observer to track visibility of sections
   function observeSections() {
     const sectionObserver = new IntersectionObserver(handleSectionVisibility, {
       root: null,
       threshold: 0.25,
     });
-
-    document.querySelectorAll("._page").forEach((section) => sectionObserver.observe(section));
+    document
+      .querySelectorAll("._page")
+      .forEach((section) => sectionObserver.observe(section));
     const footer = document.querySelector("footer");
     if (footer) sectionObserver.observe(footer);
   }
 
-  // Function to get predefined slide data for the first carousel
   function getSlidesData() {
     return [
       { src: "./assets/icons_streamers/testpacanela.webp", alt: "pacanela" },
@@ -68,34 +61,38 @@ document.addEventListener("DOMContentLoaded", () => {
     ];
   }
 
-  // Function to populate the first carousel
+  // populate the first carousel
   function populateFirstCarousel(slidesData) {
     const $blazeTrack1 = $("#image-carousel .blaze-track");
     $blazeTrack1.empty();
     slidesData.forEach((slide) => {
-      const $slideDiv = $("<div>");
+      const $slideDiv = $("<div class='pointer'>");
       const $linkElement = $("<a>");
- const $imgElement = $("<img>").attr({ src: slide.src, alt: slide.alt, loading: "lazy" });
+      const $imgElement = $("<img>").attr({
+        src: slide.src,
+        alt: slide.alt,
+        loading: "lazy",
+      });
       $linkElement.attr("target", "_blank").append($imgElement);
       $slideDiv.append($linkElement);
       $blazeTrack1.append($slideDiv);
     });
   }
 
-  // Function to fetch games data asynchronously
+  //  fetch games data asynchronously
   async function fetchGames() {
     if (gamesDataFetched) return gamesData;
-
     try {
       const response = await fetch("./games_new.json");
       if (!response.ok) throw new Error("Network response was not ok");
-
       const data = await response.json();
       if (data.status && data.data && data.data.games) {
         gamesDataFetched = true;
         return data.data.games.map((game) => ({
           ...game,
-          image_url: game.image_url ? game.image_url.replace(game.id, game.id - 1) : "",
+          image_url: game.image_url
+            ? game.image_url.replace(game.id, game.id - 1)
+            : "",
         }));
       } else {
         console.log("No games found or API returned an error.");
@@ -107,21 +104,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // Function to populate the second carousel
+  // populate the second carousel
   function populateSecondCarousel(gamesData) {
     const $blazeTrack2 = $("#games-carousel .blaze-track");
     $blazeTrack2.empty();
-  
     gamesData.forEach((game, index) => {
       if (game.image_url && game.game_url) {
         const gameElement = `
           <div class="game relative" data-index="${index}">
             <div class="parent-game d-flex-center flex-between flex-col h-full">
-              <a href="${game.game_url}" target="_blank">
+              <a href="${game.game_url}" target="_blank" class="pointer">
                 <!-- Adding loading="lazy" attribute to the image -->
                 <img src="${game.image_url}" alt="${game.name}" loading="lazy">
               </a>
-              <div class="footer-game">
+              <div class="footer-game pointer">
                 <div class="footer-text">
                   <p>PragmaticPlay</p>
                   <p><strong>${game.name}</strong></p>
@@ -133,9 +129,8 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
-  
 
-  // Function to initialize a BlazeSlider carousel
+  // initialize BlazeSlider carousel
   function initializeCarousel(selector, options) {
     const sliderElement = $(selector)[0];
     if (sliderElement) {
@@ -143,7 +138,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // Get the first carousel configuration options
+  // first carousel config options
   function getFirstCarouselOptions() {
     return {
       all: {
@@ -158,7 +153,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-  // Get the second carousel configuration options
+  // second carousel config options
   function getSecondCarouselOptions() {
     return {
       all: {
@@ -172,19 +167,4 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     };
   }
-
-  // Setup Scroll to Top button
-  // function setupScrollToTop() {
-  //   $("#scrollToTop").on("click", function () {
-  //     $("html, body").animate({ scrollTop: $("#section1").offset().top }, "slow");
-  //   });
-
-  //   $(window).scroll(function () {
-  //     if ($(this).scrollTop() > 200) {
-  //       $("#scrollToTop").fadeIn();
-  //     } else {
-  //       $("#scrollToTop").fadeOut();
-  //     }
-  //   });
-  // }
 });
--- batalia-streamerilor-2024/dataTable.js
+++ batalia-streamerilor-2024/dataTable.js
@@ -2,77 +2,13 @@
 const token = "2f97bb641f2096c1e98a723c249a6ece";
 const url = "https://qaadmin.livepartners.com/api/streaming/";
 
-const username = "testmar";
-const netbet_id = 35115686;
+const username = "testcozminnou";
+const netbet_id = 39183753;
 
-// jQuery Selectors
 const $loading = $(".loading");
 const $optInBtn = $("#optin-btn");
 const $optOutBtn = $("#optout-btn");
 const $message = $("#message");
-
-// Debounce Function to prevent multiple rapid API calls
-function debounce(func, delay) {
-  let debounceTimer;
-  let rejectLastPromise;
-
-  return function (...args) {
-    if (debounceTimer) {
-      clearTimeout(debounceTimer);
-    }
-    if (rejectLastPromise) {
-      rejectLastPromise("Debounced");
-    }
-
-    return new Promise((resolve, reject) => {
-      rejectLastPromise = reject;
-
-      debounceTimer = setTimeout(() => {
-        func.apply(this, args).then(resolve).catch(reject);
-      }, delay);
-    });
-  };
-}
-
-// Generate stars based on ranking
-function generateStars(ranking) {
-  let stars;
-
-  if (ranking === 1) {
-    stars = 5;
-  } else if (ranking <= 5) {
-    stars = 4;
-  } else if (ranking <= 9) {
-    stars = 3;
-  } else if (ranking <= 13) {
-    stars = 2;
-  } else {
-    stars = 1;
-  }
-  // if (ranking === 1) {
-  //   return "<span><img src='./assets/star-icon-png-8.jpg'></span>".repeat(stars);
-  // }
-  // Create star icons dynamically
-  return "&#9733;".repeat(stars);
-}
-
-// Function to create table rows for users
-function createTableUsers(item, userPosition) {
-  const highlightClass = item.id === netbet_id ? "highlight-row" : "";
-
-  const tableAHtml = `
-    <tr class="parent-table ${highlightClass}">
-        <td class="parent-position ps">#${userPosition}</td>
-       
-        <td>
-          <div class="parent-name">
-            <p class="ps">${item.username}</p>
-          </div>
-        </td>
-        <td class="parent-points ps">${item.points}</td>  
-    </tr>`;
-  return tableAHtml;
-}
 
 const streamerImages = {
   pacanela: "./assets/icons_table/pacanela.webp",
@@ -92,7 +28,69 @@ const streamerImages = {
   sorin: "./assets/icons_table/sorin.webp",
 };
 
-// Function to create table rows for streamers with star ratings
+// prevent multiple rapid API calls
+function debounce(func, delay) {
+  let debounceTimer;
+  let rejectLastPromise;
+
+  return function (...args) {
+    if (debounceTimer) {
+      clearTimeout(debounceTimer);
+    }
+    if (rejectLastPromise) {
+      rejectLastPromise("Debounced");
+    }
+    return new Promise((resolve, reject) => {
+      rejectLastPromise = reject;
+      debounceTimer = setTimeout(() => {
+        func.apply(this, args).then(resolve).catch(reject);
+      }, delay);
+    });
+  };
+}
+// Format Points
+function formatPoints(points) {
+  if (points === null || points === undefined) {
+    return "0";
+  }
+  points = Number(points);
+  if (isNaN(points)) {
+    return "0";
+  }
+  if (points < 1000) {
+    return points.toFixed(0);
+  } else if (points < 1000000) {
+    return (points / 1000).toFixed(1) + "K";
+  } else if (points < 1000000000) {
+    return (points / 1000000).toFixed(1) + "M";
+  } else {
+    return (points / 1000000000).toFixed(1) + "B";
+  }
+}
+// Generate stars based on ranking
+function generateStars(ranking) {
+  let stars;
+  if (ranking === 1) {
+    stars = 5;
+  } else if (ranking <= 5) {
+    stars = 4;
+  } else if (ranking <= 9) {
+    stars = 3;
+  } else if (ranking <= 13) {
+    stars = 2;
+  } else {
+    stars = 1;
+  }
+
+  let starIcons = "&#9733;".repeat(stars);
+
+  if (ranking === 1) {
+    return starIcons + "<span><img src='./assets/wanted.webp'></span>";
+  }
+  return starIcons;
+}
+
+// create table for streamers
 function createTableStreamers(streamer) {
   let displayName;
   switch (streamer.username.toLowerCase()) {
@@ -135,9 +133,9 @@ function createTableStreamers(streamer) {
     case "annuskaa":
       displayName = "anna";
       break;
-      case "sisha1487":
-        displayName = "sorin";
-        break;
+    case "sisha1487":
+      displayName = "sorin";
+      break;
     default:
       displayName = "noName";
   }
@@ -148,11 +146,11 @@ function createTableStreamers(streamer) {
   const highlightClass = streamer.id === netbet_id ? "highlight-row" : "";
 
   const tableHtml = `
-    <tr class="parent-table ${highlightClass}">
+    <tr class="parent-table ${highlightClass} pointer">
         <td class="parent-position ps">#${streamer.ranking}</td>
         <td>
           <div class="d-flex-start flex-row parent-avatar">
-            <div class="avatar-table">
+            <div class="avatar-table ">
               <img src="${imgSrc}" alt="pict_table" class="pict_table">
             </div>
             <div class="parent-name">
@@ -166,89 +164,6 @@ function createTableStreamers(streamer) {
   return tableHtml;
 }
 
-// Format Points
-function formatPoints(points) {
-  if (points === null || points === undefined) {
-    return "0";
-  }
-  points = Number(points);
-  if (isNaN(points)) {
-    return "0";
-  }
-  if (points < 1000) {
-    return points.toFixed(0);
-  } else if (points < 1000000) {
-    return (points / 1000).toFixed(1) + "K";
-  } else if (points < 1000000000) {
-    return (points / 1000000).toFixed(1) + "M";
-  } else {
-    return (points / 1000000000).toFixed(1) + "B";
-  }
-}
-
-// Generate tables and populate with API data
-function generateTables(userData, streamerData) {
-  let tableDataUser = $("#bodyUser");
-  let tableDataStreamer = $("#bodyStreamer");
-  // Clear previous data
-  tableDataUser.empty();
-  tableDataStreamer.empty();
-  
-  // Populate user data
-  userData.forEach((user, index) => {
-    const userRow = createTableUsers(user, index + 1);
-    tableDataUser.append(userRow);
-  });
-
-  // Populate streamer data with stars
-  streamerData.forEach((streamer) => {
-    const streamerRow = createTableStreamers(streamer);
-    tableDataStreamer.append(streamerRow);
-  });
-
-  // Initialize DataTables
-  $("#usersTable").DataTable({
-    autoWidth: false,
-    aaSorting: false,
-    responsive: false,
-    pageLength: 10,
-    paginate: true,
-    info: false,
-    searching: true,
-    lengthChange: false,
-    language: false,
-    pagingType: "simple_numbers",
-    columnDefs: [
-      { width: "10%", targets: 0, className: "dt-center" },
-      { width: "45%", targets: 1, className: "dt-center" },
-      { width: "30%", targets: 2, className: "dt-right" },
-    ],
-    destroy: true,
-  });
-
-  $("#streamersTable").DataTable({
-    autoWidth: false,
-    aaSorting: false,
-    responsive: false,
-    pageLength: 7,
-    paginate: true,
-    language: false,
-    lengthChange: false,
-    info: false,
-    searching: false,
-    columnDefs: [
-      { width: "8%", targets: 0 },
-      { width: "60%", targets: 1 },
-      { width: "14%", targets: 2 },
-    ],
-    destroy: true,
-  });
-
-  // Move pagination to custom locations
-  $("#usersTable_paginate").detach().appendTo("._users");
-  $("#streamersTable_paginate").detach().appendTo("._streamers");
-}
-
 // Fetch User Data and Check if the Player Exists
 async function fetchDataUsers() {
   try {
@@ -260,36 +175,109 @@ async function fetchDataUsers() {
       },
     });
 
+    if (!response || !Array.isArray(response.data)) {
+      console.error("Invalid or missing data in response");
+      return [];
+    }
+
+    const totalUsers = response.data.length;
+    const currentUserIndex = response.data.findIndex(
+      (player) => player.id === netbet_id
+    );
+    const isUserInTable = currentUserIndex !== -1;
+
+    // Top 8 users
+    const top8 = response.data.slice(0, 8);
+    const top3 = response.data.slice(0, 3);
+    // Last 2 users
+    const last2 = response.data.slice(-2);
+    const last3 = response.data.slice(-3);
+
+    let combinedUsers = [];
+
+    // Handle specific cases based on user position
+    if (currentUserIndex === 0) {
+      // Case 1: User is in the first place
+      combinedUsers = [
+        ...response.data.slice(currentUserIndex, currentUserIndex + 1), // User at first place
+        ...response.data.slice(1, 8), // Top 8 excluding the first user
+        ...last2, // Last 2 users
+      ];
+    } else if (currentUserIndex > 0 && currentUserIndex <= 2) {
+      // Case 2: User is in the top 3
+      combinedUsers = [
+        ...top8, // Top 8 users
+        ...last2, // Last 2 users
+      ];
+    } else if (currentUserIndex === totalUsers - 1) {
+      // Case 3: User is in the last place
+      const neighbors = response.data.slice(
+        Math.max(0, currentUserIndex - 1),
+        currentUserIndex + 1
+      );
+      combinedUsers = [
+        ...top8, // Top 8 users
+        ...neighbors, // User and neighbors at the bottom
+        ...last2, // Last 2 users
+      ];
+    } else if (!isUserInTable) {
+      // Case 4: User is not in the table
+      combinedUsers = [
+        ...top8, // Top 8 users
+        ...last2, // Last 2 users
+      ];
+    } else {
+      // Case 5: General case (user somewhere in the middle)
+      const neighbors = response.data.slice(
+        Math.max(0, currentUserIndex - 1),
+        currentUserIndex + 2
+      );
+      combinedUsers = [
+        ...top3, // Top 8 users
+        ...neighbors, // User and neighbors
+        ...last3, // Last 2 users
+      ];
+    }
+
+    // Remove duplicates using Set and map back to objects
+    combinedUsers = [...new Set(combinedUsers)];
+    console.log(combinedUsers);
     const userExists = response.data.some((player) => player.id === netbet_id);
-    toggleButtons(userExists); // Enable/disable buttons based on user's existence
-    return response.data.map((player, index) => ({
-      ranking: index + 1,
-      id: player.id,
-      username: player.id === netbet_id ? username : player.username,
-      points: formatPoints(player.points),
-    }));
+    toggleButtons(userExists);
+
+    // Return the filtered users with their actual ranking from the full list
+    return combinedUsers.map((player) => {
+      const actualRanking =
+        response.data.findIndex((p) => p.id === player.id) + 1;
+      return {
+        ranking: actualRanking, // Actual position from full list
+        id: player.id,
+        username: player.id === netbet_id ? username : player.username,
+        points: formatPoints(player.points),
+      };
+    });
   } catch (error) {
     console.error("Error fetching player data:", error);
   }
 }
-//                function addStreamers(username) {
-//   $.ajax({
-//     url: url + 'streamer/' + username + '/' + 1,
-//     type: 'POST',
-//     headers: {
-//         'Authorization': 'Bearer ' + token
-//     },
-//     success: function(response) {
-//         console.log('response stre:', response);
-//     },
-//     error: function(xhr, status, error) {
-//         console.log('response:', error);
-//     }
-// });
-//  }
 
-//  addStreamers(username);
-// Fetch Streamer Data
+// create table for users
+function createTableUsers(item) {
+  const highlightClass = item.id === netbet_id ? "highlight-row" : "";
+  const tableAHtml = `
+    <tr class="parent-table ${highlightClass} pointer">
+        <td class="parent-position ps">#${item.ranking}</td>
+        <td>
+          <div class="parent-name">
+            <p class="ps">${item.username}</p>
+          </div>
+        </td>
+        <td class="parent-points ps">${item.points}</td>  
+    </tr>`;
+  return tableAHtml;
+}
+
+// Fetch Streamers Data
 async function fetchStreamerData() {
   try {
     const response = await $.ajax({
@@ -354,13 +342,6 @@ async function optOutPlayer(username) {
   }
 }
 
-// Enable or Disable Buttons based on player existence
-function toggleButtons(isOptedIn) {
-  $optInBtn.prop("disabled", isOptedIn);
-  $optOutBtn.prop("disabled", !isOptedIn);
-}
-
-// Reload the User Table
 async function reloadUserTable() {
   try {
     const userData = await fetchDataUsers();
@@ -370,11 +351,94 @@ async function reloadUserTable() {
     console.error("Error reloading user table:", error);
   }
 }
+// Enable or Disable Buttons based on player existence
 
-// Initialize tables on document ready
+function generateTables(userData, streamerData) {
+  let tableDataUser = $("#bodyUser");
+  let tableDataStreamer = $("#bodyStreamer");
+  tableDataUser.empty();
+  tableDataStreamer.empty();
+
+  userData.forEach((user, index) => {
+    const userRow = createTableUsers(user, index + 1);
+    tableDataUser.append(userRow);
+  });
+
+  streamerData.forEach((streamer) => {
+    const streamerRow = createTableStreamers(streamer);
+    tableDataStreamer.append(streamerRow);
+  });
+
+  // Init DataTables
+  $("#usersTable").DataTable({
+    autoWidth: false,
+    aaSorting: false,
+    responsive: false,
+    pageLength: 10,
+    paginate: true,
+    info: false,
+    searching: true,
+    lengthChange: false,
+    language: false,
+    pagingType: "simple_numbers",
+    columnDefs: [
+      { width: "10%", targets: 0, className: "dt-center" },
+      { width: "45%", targets: 1, className: "dt-center" },
+      { width: "30%", targets: 2, className: "dt-right" },
+    ],
+    destroy: true,
+  });
+
+  $("#streamersTable").DataTable({
+    autoWidth: false,
+    aaSorting: false,
+    responsive: false,
+    pageLength: 7,
+    paginate: true,
+    language: false,
+    lengthChange: false,
+    info: false,
+    searching: false,
+    columnDefs: [
+      { width: "8%", targets: 0 },
+      { width: "60%", targets: 1 },
+      { width: "14%", targets: 2 },
+    ],
+    destroy: true,
+  });
+
+  // Move pagination
+  $("#usersTable_paginate").detach().appendTo("._users");
+  $("#streamersTable_paginate").detach().appendTo("._streamers");
+}
+
+// Reload the User Table
+
+//                function addStreamers(username) {
+//   $.ajax({
+//     url: url + 'streamer/' + username + '/' + 1,
+//     type: 'POST',
+//     headers: {
+//         'Authorization': 'Bearer ' + token
+//     },
+//     success: function(response) {
+//         console.log('response stre:', response);
+//     },
+//     error: function(xhr, status, error) {
+//         console.log('response:', error);
+//     }
+// });
+//  }
+
+//  addStreamers(username);
+function toggleButtons(isOptedIn) {
+  $optInBtn.prop("disabled", isOptedIn);
+  $optOutBtn.prop("disabled", !isOptedIn);
+}
+// Initialize on document ready
 $(document).ready(function () {
   reloadUserTable();
-  // Set up click events for opt-in and opt-out buttons
-  $optInBtn.click(debounce(() => optInPlayer(username), 300));
-  $optOutBtn.click(debounce(() => optOutPlayer(username), 300));
+
+  $optInBtn.click(debounce(() => optInPlayer(username), 400));
+  $optOutBtn.click(debounce(() => optOutPlayer(username), 400));
 });
